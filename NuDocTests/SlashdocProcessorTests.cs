8a26ff02e35d1f313f2bd1c9db0075d1ab8e132f
anderskaplan <anderskaplan@magasin1.se>
2013-01-08 20:53:49 +0100

    Removed dependency on SampleAssembly from SlashdocProcessorTests.

--- NuDocTests/SlashdocProcessorTests.cs
+++ NuDocTests/SlashdocProcessorTests.cs
@@ -6,6 +6,7 @@
     using System.Xml.XPath;
     using NuDoc;
     using NUnit.Framework;
+    using Moq;
 
     [TestFixture]
     public class SlashdocProcessorTests
@@ -17,10 +18,12 @@
 
             File.Delete(fileName);
 
-            var assembly = new AssemblyReflector(@"../../../SampleAssembly/bin/Debug/SampleAssembly.dll", new ConsoleLogger());
-            using (var slashdocStream = new FileStream(@"../../../SampleAssembly/bin/Debug/SampleAssembly.xml", FileMode.Open, FileAccess.Read))
+            using (var assembly = new AssemblyReflector(@"NuDocTests.exe", new Mock<ILog>().Object))
             {
-                SlashdocProcessor.WritePublicApiSlashdoc(assembly, slashdocStream, fileName);
+                using (var slashdocStream = new FileStream(@"NuDocTests.xml", FileMode.Open, FileAccess.Read))
+                {
+                    SlashdocProcessor.WritePublicApiSlashdoc(assembly, slashdocStream, fileName);
+                }
             }
 
             using (var xmlReader = XmlReader.Create(new FileStream(fileName, FileMode.Open, FileAccess.Read)))
@@ -28,23 +31,23 @@
                 var doc = new XPathDocument(xmlReader);
                 var navigator = doc.CreateNavigator();
                 Assert.That(navigator.SelectSingleNode("/doc"), Is.Not.Null);
-                Assert.That(navigator.SelectSingleNode("/doc/assembly/name/text()").Value, Is.EqualTo("SampleAssembly"), "assembly name is preserved.");
+                Assert.That(navigator.SelectSingleNode("/doc/assembly/name/text()").Value, Is.EqualTo("NuDocTests"), "assembly name is preserved.");
                 
                 Assert.That(
-                    InnerXmlWithoutWhitespace(navigator.SelectSingleNode("/doc/members/member[@name='T:SampleAssembly.Class1']")), 
+                    InnerXmlWithoutWhitespace(navigator.SelectSingleNode("/doc/members/member[@name='T:TestData.Xyz.Foo.SlashdocTestClass']")), 
                     Is.EqualTo("<summary>Aclass.</summary>"),
-                    "slashdoc for the public type Class1 is preserved.");
+                    "slashdoc for the public type SlashdocTestClass is preserved.");
                 Assert.That(
-                    InnerXmlWithoutWhitespace(navigator.SelectSingleNode("/doc/members/member[@name='M:SampleAssembly.Class1.DoSomething(System.DateTime)']")), 
+                    InnerXmlWithoutWhitespace(navigator.SelectSingleNode("/doc/members/member[@name='M:TestData.Xyz.Foo.SlashdocTestClass.DoSomething(System.DateTime)']")), 
                     Is.EqualTo("<summary>IfIcouldonlyrememberwhat.</summary><paramname=\"when\">Andwhen.</param>"),
-                    "slashdoc for the public member Class1.DoSomething is preserved.");
+                    "slashdoc for the public member SlashdocTestClass.DoSomething is preserved.");
                 Assert.That(
-                    navigator.SelectSingleNode("/doc/members/member[@name='M:SampleAssembly.Class1.PrivateMethod']"), 
+                    navigator.SelectSingleNode("/doc/members/member[@name='M:TestData.Xyz.Foo.SlashdocTestClass.PrivateMethod']"), 
                     Is.Null, 
-                    "slashdoc for the private member Class1.PrivateMethod is NOT preserved.");
+                    "slashdoc for the private member SlashdocTestClass.PrivateMethod is NOT preserved.");
 
                 Assert.That(
-                    navigator.SelectSingleNode("/doc/members/member[@name='T:SampleAssembly.InternalClass']"), 
+                    navigator.SelectSingleNode("/doc/members/member[@name='T:TestData.Xyz.Foo.InternalClass']"), 
                     Is.Null, 
                     "slashdoc for the non-public type InternalClass is NOT preserved.");
 
--- NuDocTests/TestClass.cs
+++ NuDocTests/TestClass.cs
@@ -324,4 +324,51 @@
 
         public event EventHandler AnEvent;
     }
+
+    /// <summary>
+    /// A class.
+    /// </summary>
+    public class SlashdocTestClass
+    {
+        /// <summary>
+        /// An important property. <see cref="SampleAssembly"/> Yes box allright.
+        /// </summary>
+        public bool Foo { get; set; }
+
+        /// <summary>
+        /// If I could only remember what.
+        /// </summary>
+        /// <param name="when">And when.</param>
+        public void DoSomething(DateTime when)
+        {
+        }
+
+        /// <summary>
+        /// Test for equality.
+        /// </summary>
+        public static bool operator ==(SlashdocTestClass first, SlashdocTestClass second)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Test for inequality.
+        /// </summary>
+        public static bool operator !=(SlashdocTestClass first, SlashdocTestClass second)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Delegate type.
+        /// </summary>
+        public delegate int Frotz(int x);
+
+        /// <summary>
+        /// A private method.
+        /// </summary>
+        private void PrivateMethod()
+        {
+        }
+    }
 }
