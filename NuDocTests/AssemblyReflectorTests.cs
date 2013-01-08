3e2ee20d9812e11382f023e45afe81f9edc70092
anderskaplan <anderskaplan@magasin1.se>
2013-01-08 20:45:14 +0100

    Removed dependency on SampleAssembly from AssemblyReflectorTests.

--- NuDocTests/AssemblyReflectorTests.cs
+++ NuDocTests/AssemblyReflectorTests.cs
@@ -6,6 +6,7 @@
     using System.Text;
     using NUnit.Framework;
     using NuDoc;
+    using Moq;
 
     [TestFixture]
     public class AssemblyReflectorTests
@@ -13,15 +14,16 @@
         [Test]
         public void ShouldLookTypesByName()
         {
-            var assembly = new AssemblyReflector(@"../../../SampleAssembly/bin/Debug/SampleAssembly.dll", new ConsoleLogger());
-            
-            Assert.That(assembly.LookupType("System.Guid"), Is.Null, "Lookup of a type which isn't in the assembly returns null.");
+            using (var assembly = new AssemblyReflector(@"NuDocTests.exe", new Mock<ILog>().Object))
+            {
+                Assert.That(assembly.LookupType("System.Guid"), Is.Null, "Lookup of a type which isn't in the assembly returns null.");
 
-            var class1 = assembly.LookupType("SampleAssembly.Class1");
-            Assert.That(class1, Is.Not.Null);
+                var class1 = assembly.LookupType("TestData.Xyz.Foo.TestClass");
+                Assert.That(class1, Is.Not.Null, "TestClass lookup succeeds.");
 
-            var genericClass = assembly.LookupType("SampleAssembly.GenericClass`2");
-            Assert.That(genericClass, Is.Not.Null);
+                var genericClass = assembly.LookupType("TestData.Xyz.Foo.TestGeneric`2");
+                Assert.That(genericClass, Is.Not.Null, "Generic class lookup succeeds.");
+            }
         }
     }
 }
